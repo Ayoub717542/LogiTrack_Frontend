3d30ece7159875147a854801e9664099561d45c6
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-08-08 17:01:32 +0100

    update app jsx in develop branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
-import Login from "./pages/Login"
-import Register from "./pages/Register";
-import { Route, Routes  } from "react-router-dom";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
 
-
-function App() {
-  return (
-    <>
-     <Routes>
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />}/>
-   </Routes>
-    <ToastContainer />
-    </>
-  
-  )
-=======
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./security/ProtectedRoute";
+import Login from "./pages/Login"
+import Register from "./pages/Register";
 
 function App() {
     return (
         <Routes>
 
-            {/* Public routes */}
-            {/* Login and Register will be here */}
+            <Route path="login" element={<Login/>}/>
+            <Route path="register" element={<Register/>}/>
 
             {/* Everything inside this route requires authentication */}
             <Route element={<ProtectedRoute />}>
@@ -37,7 +20,6 @@ function App() {
 
         </Routes>
     );
->>>>>>> security
 }
 
 export default App;
